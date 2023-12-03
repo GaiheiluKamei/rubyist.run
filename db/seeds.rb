1368c223c7c4aaee80e76bcc6b50d112a8c6698e
GaiheiluKamei <love@gaiheilukamei.cc>
2023-12-03 22:27:09 +0800

    change seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 puts "\n== Seeding the database with fixtures =="
-system("bin/rails db:fixtures:load")
+# system("bin/rails db:fixtures:load")
+# ActiveRecord::Fixture::FormatError: YAML syntax error occurred
+
+user = User.create!(email: 'gk@example.com', password: 'password')
+
+10.times do |i|
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body_markdown: Faker::Markdown.sandwich(sentences: 10, repeat: 3),
+    published_at: Time.current,
+    user: user,
+  )
+end
