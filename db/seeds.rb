3b1e1a3c1ae2ae5de74bfad30095c1f3d1e6d1a8
GaiheiluKamei <cc@gaiheilukamei.cc>
2024-01-23 16:18:42 +0800

    don't run seed in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-puts "\n== Seeding the database with fixtures =="
-# system("bin/rails db:fixtures:load")
-# ActiveRecord::Fixture::FormatError: YAML syntax error occurred
+unless ENV['DISABLE_SEED'] == 'true'
+  puts "\n== Seeding the database with fixtures =="
+  # system("bin/rails db:fixtures:load")
+  # ActiveRecord::Fixture::FormatError: YAML syntax error occurred
+  user = User.create!(email: 'gk@example.com', password: 'password')
 
-user = User.create!(email: 'gk@example.com', password: 'password')
-
-10.times do |i|
-  Post.create!(
-    title: Faker::Book.title,
-    body_markdown: Faker::Markdown.sandwich(sentences: 10, repeat: 3),
-    published_at: Time.current,
-    user: user,
-  )
+  10.times do |i|
+    Post.create!(
+      title: Faker::Book.title,
+      body_markdown: Faker::Markdown.sandwich(sentences: 10, repeat: 3),
+      published_at: Time.current,
+      user: user,
+      )
+  end
 end
